04b7463d4c659097374d94fc96e5436838c0110e
sunli <sunlightsvett@gmail.com>
2022-07-25 21:04:35 +0300

    Drawing App

--- app/src/main/java/com/example/drawingapp/DrawingView.kt
+++ app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -1,7 +1,7 @@
 package com.example.drawingapp
 
 import android.content.Context
-import android.graphics.Path
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 
@@ -9,8 +9,30 @@ import android.view.View
 class DrawingView(context : Context, attrs : AttributeSet) : View(context , attrs) {
 
     private var mDrawPath : CustomPath? = null
+    private var mCanvasBitmap: Bitmap? = null
+    private var mDrawPaint: Paint? = null
+    private var mCanvasPaint: Paint? = null
+    private var mBrushSize: Float = 0.toFloat()
+    private var color = Color.BLACK
+    private var canvas: Canvas? = null
 
-    internal inner class CustomPath : Path() {
+    init{
+        setupDrawing()
+    }
+
+    private fun setupDrawing(){
+        mDrawPaint = Paint()
+        mDrawPaint!!.color = color
+        mDrawPaint!!.style = Paint.Style.STROKE
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+
+        mDrawPath = CustomPath(color,mBrushSize)
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+        mBrushSize = 20.toFloat()
+    }
+
+    internal inner class CustomPath(var color: Int,var brushThickness: Float) : Path() {
 
     }
 }
