520112edc4fa43c8824e11c5e94d2cc39c65e1b1
sunli <sunlightsvett@gmail.com>
2022-08-02 21:25:17 +0300

    Drawing App

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -3,16 +3,31 @@ package com.example.drawingapp
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
+    private var mImageButtonCurrentPaint: ImageButton? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(20.toFloat())
+        val linearLayoutPaintColors = findViewById<LinearLayout>(R.id.ll_paint_colors)
+        mImageButtonCurrentPaint = linearLayoutPaintColors[2] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(
+           ContextCompat.getDrawable(this,R.drawable.pallet_pressed)
+        )
+        val ib_brush : ImageButton = findViewById(R.id.ib_brush)
+        ib_brush.setOnClickListener(){
+            showBrushSizeChooserDialog()
+        }
     }
 
     private fun showBrushSizeChooserDialog(){
@@ -24,7 +39,16 @@ class MainActivity : AppCompatActivity() {
             drawingView?.setSizeForBrush(10.toFloat())
             brushDialog.dismiss()
         }
-
+        val mediumBtn : ImageButton = brushDialog.findViewById(R.id.ib_medium_brush)
+        mediumBtn.setOnClickListener{
+            drawingView?.setSizeForBrush(20.toFloat())
+            brushDialog.dismiss()
+        }
+        val largeBtn : ImageButton = brushDialog.findViewById(R.id.ib_large_brush)
+        largeBtn.setOnClickListener{
+            drawingView?.setSizeForBrush(30.toFloat())
+            brushDialog.dismiss()
+        }
         brushDialog.show()
     }
 }
